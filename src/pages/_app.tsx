488cea087236b97a8d7bd087e58968a1eb1f3135
Daniel Firebanks-Quevedo <dafirebanks@gmail.com>
2023-05-06 00:32:25 -0700

    formatted files, created new components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,9 +6,11 @@ import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <ClerkProvider {...pageProps}>
-            <Component {...pageProps} />
-          </ClerkProvider>;
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
