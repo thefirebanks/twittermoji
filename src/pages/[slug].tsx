4f8487a6f565d801985dd45a24c0a53c7b651890
Daniel Firebanks-Quevedo <dafirebanks@gmail.com>
2023-05-07 14:54:44 -0700

    Added workarounds to load username and posts individually from query in URL

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,14 +1,12 @@
 import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import { api } from "~/utils/api";
-import { createServerSideHelpers } from "@trpc/react-query/server";
-import { appRouter } from "~/server/api/root";
-import { prisma } from "~/server/db";
-import superjson from "superjson";
 import { PageLayout } from "~/components/layout";
 import Image from "next/image";
 import { LoadingPage } from "~/components/loading";
 import { PostView } from "~/components/postview";
+import { generateSSGHelper } from "~/server/helpers/ssgHelper";
+import { useRouter } from "next/router";
 
 const ProfileFeed = (props: { userId: string }) => {
   const { data, isLoading } = api.posts.getPostsByUserId.useQuery({
@@ -28,9 +26,16 @@ const ProfileFeed = (props: { userId: string }) => {
   );
 };
 
-const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
+const ProfilePage: NextPage = () => {
+  // NOTE: TEMPORARY WORKOUROUND TO GET USERNAME FROM URL: useRouter()
+  const router = useRouter();
+  const slug = router.query?.slug;
+  if (typeof slug !== "string") throw new Error("no slug");
+  const username = slug?.replace("@", "");
+  // END OF TEMPORARY WORKAROUND
+
   const { data } = api.profile.getUserByUsername.useQuery({
-    username: "thefirebanks",
+    username,
   });
 
   if (!data) return <div>404</div>;
@@ -63,18 +68,14 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const ssg = createServerSideHelpers({
-    router: appRouter,
-    ctx: { prisma, userId: null },
-    transformer: superjson,
-  });
+  const ssg = generateSSGHelper();
 
-  const slug = context.params?.slug as string;
+  const slug = context.params?.slug;
   if (typeof slug !== "string") throw new Error("no slug");
 
   const username = slug?.replace("@", "");
 
-  await ssg.profile.getUserByUsername.fetch({ username: username });
+  await ssg.profile.getUserByUsername.prefetch({ username });
 
   return {
     props: {
